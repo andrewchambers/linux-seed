d5359a0fb4f195713beab5e11e0aff38deb3354d
Andrew Chambers <ac@acha.ninja>
2019-07-23 01:31:48 +1200

    Don't build as root.

--- support/ci.sh
+++ support/ci.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 set -eu
 
+sudo mkdir -p /opt/linux-seed
+sudo chown /opt/linux-seed "$(whoami)"
 echo "FINAL_OUTPUT=\"/opt/linux-seed/\"" > config.inc
-sudo nix-shell --pure ./support/shell.nix --run "/bin/sh ./build.sh"
+nix-shell --pure ./support/shell.nix --run "/bin/sh ./build.sh"
