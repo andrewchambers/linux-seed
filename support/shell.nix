7a41f82b2270733e003c31d9296c58c783c8f1d8
Andrew Chambers <ac@acha.ninja>
2019-07-22 12:51:35 +1200

    Work on CI.

--- support/shell.nix
+++ support/shell.nix
@@ -1,7 +1,10 @@
 /* This file defines the build environment used for testing.
    You can ignore it if you don't know what nix is. */
 let 
-  pkgs = (import <nixpkgs> {});
+  pkgs = import (builtins.fetchTarball {
+    url = https://github.com/nixos/nixpkgs/archive/e467600691717e2196ae377c8b189894d4a0cbb9.tar.gz;
+    sha256 = "058dj7dm0ix0ky9lw4588xn94mxi6h8ayckmpv2gphn1r5gnhidf";
+  }) {};
 in
   pkgs.mkShell {
       buildInputs = with pkgs; [ 
